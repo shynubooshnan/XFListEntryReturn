aff0ab39b5bc49270206fa9964b526c2cfdd542c
Shynu <shynu.bp@landmarkgroup.com>
2021-11-17 20:20:53 +0530

    collection view ui updated

--- ListViewKeyboardEntry/MainPage.xaml.cs
+++ ListViewKeyboardEntry/MainPage.xaml.cs
@@ -12,6 +12,8 @@ namespace ListViewKeyboardEntry
     public partial class MainPage : ContentPage
     {
         #region variables
+        private int _nextIndex;
+
         private List<Cell> _collectionListCell { get; set; }
 
         private List<Item> _quantityList { get; set; }
@@ -22,6 +24,17 @@ namespace ListViewKeyboardEntry
             {
                 _quantityList = value;
                 OnPropertyChanged("QuantityList");
+            }
+        }
+
+        private double _collectionViewHeight { get; set; }
+        public double CollectionViewHeight
+        {
+            get => _collectionViewHeight;
+            set
+            {
+                _collectionViewHeight = value;
+                OnPropertyChanged("CollectionViewHeight");
             }
         }
 
@@ -37,11 +50,23 @@ namespace ListViewKeyboardEntry
                 new Item{Title = "Quantity2", Quantity = "0", EntryId = 2},
                 new Item{Title = "Quantity3", Quantity = "0", EntryId = 3},
                 new Item{Title = "Quantity4", Quantity = "0", EntryId = 4},
-                new Item{Title = "Quantity5", Quantity = "0", EntryId = 5}
+                new Item{Title = "Quantity5", Quantity = "0", EntryId = 5},
+                new Item{Title = "Quantity6", Quantity = "0", EntryId = 6},
+                new Item{Title = "Quantity7", Quantity = "0", EntryId = 7},
+                new Item{Title = "Quantity8", Quantity = "0", EntryId = 8},
+                new Item{Title = "Quantity9", Quantity = "0", EntryId = 9},
+                new Item{Title = "Quantity10", Quantity = "0", EntryId = 10},
+                new Item{Title = "Quantity11", Quantity = "0", EntryId = 11},
+                new Item{Title = "Quantity12", Quantity = "0", EntryId = 12},
+                new Item{Title = "Quantity13", Quantity = "0", EntryId = 13},
+                new Item{Title = "Quantity14", Quantity = "0", EntryId = 14},
+                new Item{Title = "Quantity15", Quantity = "0", EntryId = 15}
             };
-            this.BindingContext = this;
-        }
 
+            //set collection view height
+            CollectionViewHeight = QuantityList.Count * 40;
+            BindingContext = this;
+        }
 
         /// <summary>
         /// Text entry Completed Event
@@ -52,19 +77,25 @@ namespace ListViewKeyboardEntry
         {
             if (sender is Entry entry)
             {
+                _nextIndex = 0;
                 var list = (entry?.Parent?.Parent as CollectionView)?.LogicalChildren;
                 if (list == null)
                     return;
-                var index = Convert.ToInt32(entry.AutomationId) - 1;
-                if ((index + 1) >= list.Count)
+                var index = Convert.ToInt32(entry.AutomationId);
+                if ((index + 1) > list.Count)
                     return;
-                var nextIndex = index + 1;
+                _nextIndex = index + 1;
                 //Entry is added as second element on view cell grid. So, select children of element 1
-                var next = (list[nextIndex] as Grid).Children.ElementAt(1);
-                next?.Focus();
+                var nextElement = list.FirstOrDefault(x => (x as Grid).Children.ElementAt(1).AutomationId ==
+                _nextIndex.ToString());
+                if(nextElement != null)
+                {
+                    var next = (nextElement as Grid).Children.ElementAt(1);
+                    next?.Focus();
+                }
             }
-
         }
+
     }
 
 
